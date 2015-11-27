e149141472d00aae7b35a02a31a73de5a8d7b991
Julián <julian.caro@gmail.com>
2015-11-27 16:19:17 +0100

    Añadir nuevo botón para mostrar un messagebox

--- BasicMVVM/ViewModels/ViewModel.cs
+++ BasicMVVM/ViewModels/ViewModel.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace BasicMVVM.ViewModels
@@ -14,9 +15,11 @@ namespace BasicMVVM.ViewModels
     {
         private ICommand _SetTitleCommand;
         private ICommand _AddItemCommand;
+        private ICommand showYesNoQuestionCommand;
         private String _title;
         private ItemsList _items = new ItemsList();
         private Model _selectedItem;
+        private String _answer;
 
         public Model SelectedItem
         {
@@ -119,6 +122,53 @@ namespace BasicMVVM.ViewModels
             }
         }
 
+
+        public string Answer
+        {
+            get
+            {
+                return _answer;
+            }
+ 
+            set
+            {
+                _answer = value;
+                NotifyPropertyChanged("Answer");
+            }
+        }
+
+        public ICommand ShowYesNoQuestionCommand
+        {
+          get
+          {
+              this.showYesNoQuestionCommand = new RelayCommand()
+              {
+                  CanExecuteDelegate = p => true,
+                  ExecuteDelegate = p => { ShowYesNoQuestion("Are you sure?"); }
+              };
+              return this.showYesNoQuestionCommand;
+          }
+        }
+ 
+        private bool ShowYesNoQuestion(string message)
+        {
+          MessageBoxResult result = MessageBox.Show(
+              message,
+              "Question",
+              MessageBoxButton.YesNo,
+              MessageBoxImage.Question);
+ 
+          switch (result)
+          {
+              case MessageBoxResult.Yes:
+                  this.Answer = "Your answer is Yes";
+                  return true;
+ 
+              default:
+                  this.Answer = "Your answer is No";
+                  return false;
+          }
+        }
 
 
 
